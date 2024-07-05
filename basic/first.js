ffd6ef948f53d1266a9f9a6b65c29a26be59e4ce
aaassshhhiiissshhh111 <aaassshhhiiissshhh111@gmail.com>
2024-07-05 02:05:51 +0000

    hi

---
+++ basic/first.js
@@ -0,0 +1,21 @@
+console.log("Hello World, we should die");
+let a = 5;
+console.log(a);
+a = 10;
+console.log(a);
+const b = 22;
+console.log(b);
+
+let lastName = "Ashish";
+console.log(lastName);
+lastName = 6;
+console.log(lastName);
+
+console.log(4 ** 3);
+
+console.log(5 === "5");
+console.log(0 === false);
+
+
+console.log(5 == "5");
+console.log(0 == false); 

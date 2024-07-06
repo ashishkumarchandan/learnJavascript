7dc7078b67abbda05259ba83dcdd5f67093e785c
ashishkumarchandan <aaassshhhiiissshhh111@gmail.com>
2024-07-06 21:41:49 +0530

    3

--- basic/inbuildObject.js
+++ basic/inbuildObject.js
@@ -1,10 +1,18 @@
 console.log(Math.PI);
 console.log(Math.random());
-console.log(Math.round(6.8))
-console.log(Math.round(1.2))
-console.log(Math.abs(-3.8))
+console.log(Math.round(6.8));
+console.log(Math.round(1.2));
+console.log(Math.abs(-3.8));
 
 let lastName = "Kumar";
 let firstName = new String("Ashish");
 console.log(firstName, lastName);
-console.log(typeof(firstName), typeof(lastName))
+console.log(typeof firstName, typeof lastName);
+console.log(lastName.length);
+
+let message = "Let me think";
+let words = message.split(" ");
+console.log(words); 
+console.log(words[1]); 
+message = message.replace('think', 'contemplate');
+console.log(message); 

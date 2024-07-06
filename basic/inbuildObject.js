31941e69463877f1bbe677939e3aa7899060304c
ashishkumarchandan <aaassshhhiiissshhh111@gmail.com>
2024-07-06 23:46:42 +0530

    3.1

--- basic/inbuildObject.js
+++ basic/inbuildObject.js
@@ -12,7 +12,46 @@ console.log(lastName.length);
 
 let message = "Let me think";
 let words = message.split(" ");
-console.log(words); 
-console.log(words[1]); 
-message = message.replace('think', 'contemplate');
-console.log(message); 
+console.log(words);
+console.log(words[1]);
+message = message.replace("think", "contemplate");
+console.log(message);
+
+let date = new Date();
+console.log(date);
+
+let date2 = new Date(2003, 4, 10);
+console.log(date2);
+
+let numbers = [1, 4, 5, 7];
+console.log(numbers[0]);
+numbers.push(9);
+numbers.unshift(8);
+console.log(numbers);
+numbers.splice(3, 0, 2);
+console.log(numbers);
+
+console.log(numbers.includes(7));
+console.log(numbers.indexOf(4, 1));
+
+let lang = [
+  { no: 1, name: "C++" },
+  {
+    no: 2,
+    name: "JavaScript",
+  },
+];
+
+console.log(lang);
+console.log(lang.indexOf({ no: 1, name: "C++" }, 0));
+
+let isAvail = lang.find((specific) => specific.name === "JavaScript");
+console.log(isAvail);
+
+let temp = [1, 2, 3, 4, 5];
+temp.length = 0;
+console.log(temp);
+
+temp.splice(0, 0, 1, 2, 3);
+numbers = numbers.concat(temp);
+console.log(numbers);

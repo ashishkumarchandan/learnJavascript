2083e254a0207ce041cb626d127c601539a6fb2d
aaassshhhiiissshhh111 <aaassshhhiiissshhh111@gmail.com>
2024-07-06 05:11:53 +0000

    onjectOrinted

--- basic/learningObject.js
+++ basic/learningObject.js
@@ -28,22 +28,69 @@ function cRectangle() {
 }
 
 let f = cRectangle();
-f.length=3;
+f.length = 3;
 f.draw();
 
-function CreatingRectangle(){
+function CreatingRectangle() {
   this.length = 1;
   this.breadth = 2;
-  this.draw = function (){
+  this.draw = function () {
     console.log(this.length * this.breadth);
-  }
+  };
 }
 
 let red = new CreatingRectangle();
-red.length=12;
+red.length = 12;
 red.draw();
 
 red.color = "yellow";
 console.log(red);
 delete red.color;
 console.log(red);
+
+let rect = new Function(
+  "length",
+  "breadth",
+  `
+  this.length = length;
+  this.breadth = breadth;
+  this.draw = function() {
+    console.log("drawing");
+  }
+`
+);
+let r1 = new rect(2, 3);
+r1.draw();
+let d1 = new CreatingRectangle();
+d1.draw();
+console.log(d1);
+
+// let a = 10;
+// let b = a;
+// a++;
+// console.log(a, b);
+
+let a = {
+  value: 10,
+};
+let b = a;
+a.value++;
+console.log(a.value, b.value);
+
+let newRect = {
+  length: 3,
+  breadth: 5,
+};
+
+
+for(let key in newRect){
+  console.log(newRect[key] )
+}
+
+for(let key of Object.keys(newRect)){
+  console.log(key);
+}
+
+for(let key of Object.entries(newRect)){
+  console.log(key);
+}
